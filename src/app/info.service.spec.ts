63dee973566d95137b88034a6bfa239bb641db98
Jose Luis Salazar <salazarpp.js@gmail.com>
2021-10-13 21:54:40 -0500

    fix unit test

--- src/app/info.service.spec.ts
+++ src/app/info.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { InfoService } from './info.service';
@@ -6,7 +7,9 @@ describe('InfoService', () => {
   let service: InfoService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientModule]
+    });
     service = TestBed.inject(InfoService);
   });
 
--- src/app/user-login/user-login.component.spec.ts
+++ src/app/user-login/user-login.component.spec.ts
@@ -1,4 +1,6 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { UserLoginComponent } from './user-login.component';
 
@@ -8,6 +10,7 @@ describe('UserLoginComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [HttpClientModule, RouterTestingModule],
       declarations: [ UserLoginComponent ]
     })
     .compileComponents();
